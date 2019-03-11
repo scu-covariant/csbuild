2a15a72cc307c2a10b371ad1fa74431c39329f58
Michael Lee <mikecovlee@163.com>
2019-03-11 21:41:55 +0800

    Update make-deb.sh

--- deb_tools/make-deb.sh
+++ deb_tools/make-deb.sh
@@ -14,14 +14,14 @@ cp -r ../build/imports ./$2/$1/share/covscript
 cp ../build-cache/covscript/examples/*.csp ./$2/$1/share/covscript/imports
 chmod -R 777 ./$2
 SIZE=$(du -s ../build | grep -o -E "[0-9]+")
-echo "Package: covscript">>./$2/DEBIAN/control
+echo "Package: covscript-edu">>./$2/DEBIAN/control
 echo "Version: $3">>./$2/DEBIAN/control
 echo "Section: utils">>./$2/DEBIAN/control
 echo "Priority: optional">>./$2/DEBIAN/control
 echo "Architecture: $2">>./$2/DEBIAN/control
 echo "Installed-Size: $SIZE">>./$2/DEBIAN/control
 echo "Maintainer: Michael Lee <mikecovlee@163.com>">>./$2/DEBIAN/control
-echo "Description: Covariant Script Programming Language">>./$2/DEBIAN/control
+echo "Description: Covariant Script Programming Language(Education Edition)">>./$2/DEBIAN/control
 echo >>./$2/DEBIAN/control
 chmod -R 755 ./$2/DEBIAN
 dpkg-deb -b $2 covscript-$3-$2.deb
